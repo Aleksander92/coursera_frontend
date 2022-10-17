887458fde9b4f01e3e4b123028cd5438de6fd1f9
Alexander Krasnoselskikh <selskih@mail.ru>
2022-10-17 14:22:57 +0300

    test ajax

--- test_js/js/script.js
+++ test_js/js/script.js
@@ -1,7 +1,25 @@
-function f() {
-  return 69;
+function sendGetRequest(url, responseHandler) {
+  var request = new XMLHttpRequest();
+  request.onreadystatechange = function () {
+    handleResponse(request, responseHandler);
+  };
+  request.open("GET", url);
+  request.send(null);
 }
 
-function cmp(a, b) {
-  return a < b;
+function handleResponse(request, responseHandler) {
+  if (request.readyState == 4 && request.status == 200) {
+    console.log(request);
+    responseHandler(request);
+  }
 }
+
+document.addEventListener("DOMContentLoaded",
+  function (event) {
+    document.querySelector("button").addEventListener("click", function () {
+      sendGetRequest("/test_js/data/data.txt", function (request) {
+        document.querySelector("#content").innerHTML = "<h2>" + request.responseText + "</h2>";
+      });
+    });
+  }
+);
